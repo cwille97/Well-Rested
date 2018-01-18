645d749c1225aa1d83d3d77945646d88f6e807a8
Cedric <cwille97@gmail.com>
2018-01-17 22:55:42 -0500

    Added some basic button widgets
    They open a new window prompting the user to create a timer.

--- WellRested.py
+++ WellRested.py
@@ -19,3 +19,39 @@ class WellRested:
         topMenu.add_cascade(label="Window", menu=windowMenu)
         windowMenu.add_command(label="Minimize", command=print("Minimize"))
 
+        helpMenu = Menu(topMenu)
+        topMenu.add_cascade(label="Help", menu=helpMenu)
+        helpMenu.add_command(label="Getting Started")
+
+        timeElapsedLabel = Label(frame, text="Time elapsed in current interval: 0:00:00:00")
+        timeElapsedLabel.grid(row=0, column=0)
+        timeRemainingLabel = Label(frame, text="Time remaining in current interval: 0:00:00:00")
+        timeRemainingLabel.grid(row=1, column=0)
+        createIntervalButton = Button(frame, text="Create a new rest interval", command=self.createInterval)
+        createIntervalButton.grid(row=0, column=5)
+        pauseIntervalButton = Button(frame, text="Pause the current interval")
+        pauseIntervalButton.grid(row=1, column=5)
+        resetIntervalButton = Button(frame, text="Reset the current interval")
+        resetIntervalButton.grid(row=2, column=5)
+
+    def createInterval(self):
+        createIntervalFrame = Tk()
+        promptLabel = Label(createIntervalFrame, text="Please select the type of interval you would like to create")
+        promptLabel.grid(row=0)
+        defaultButton = Button(createIntervalFrame, text="Default", command=self.beginDefault and createIntervalFrame.quit)
+        defaultButton.grid(row=1, column=0)
+        customButton = Button(createIntervalFrame, text="Custom", command=self.determineCustom and createIntervalFrame.quit)
+        customButton.grid(row=1, column=1)
+        createIntervalFrame.mainloop()
+
+    def determineCustom(self):
+
+        customFrame = Tk()
+
+        customFrame.mainloop()
+
+    def beginDefault(self):
+        initialTime = 0
+
+
+
