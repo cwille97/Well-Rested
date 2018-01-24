1da3cc0a68f08216725cb6392fd4cfb1f21aa887
Cedric <cwille97@gmail.com>
2018-01-24 09:39:28 -0500

    Fixed bug where functions weren't called correctly
    It turns out that the and operator is only for logic and cannot be used to call two different functions as I had intended. Thus I created a helper function that is called by the button on command and this helper function calls the original function as well as the destroy function. I also created a few placeholder functions that will be filled out later such as pauseInterval and resetInterval.

--- WellRested.py
+++ WellRested.py
@@ -18,7 +18,7 @@ class WellRested:
 
         windowMenu = Menu(topMenu)
         topMenu.add_cascade(label="Window", menu=windowMenu)
-        windowMenu.add_command(label="Minimize", command=print("Minimize"))
+        windowMenu.add_command(label="Minimize", command=print)
 
         helpMenu = Menu(topMenu)
         topMenu.add_cascade(label="Help", menu=helpMenu)
@@ -30,18 +30,18 @@ class WellRested:
         timeRemainingLabel.grid(row=1, column=0)
         createIntervalButton = Button(frame, text="Create a new rest interval", command=self.createInterval)
         createIntervalButton.grid(row=0, column=5)
-        pauseIntervalButton = Button(frame, text="Pause the current interval")
+        pauseIntervalButton = Button(frame, text="Pause the current interval", command=self.pauseInterval)
         pauseIntervalButton.grid(row=1, column=5)
-        resetIntervalButton = Button(frame, text="Reset the current interval")
+        resetIntervalButton = Button(frame, text="Reset the current interval", command=self.resetInterval)
         resetIntervalButton.grid(row=2, column=5)
 
     def createInterval(self):
         createIntervalFrame = Tk()
         promptLabel = Label(createIntervalFrame, text="Please select the type of interval you would like to create")
         promptLabel.grid(row=0)
-        defaultButton = Button(createIntervalFrame, text="Default", command=self.beginDefault and createIntervalFrame.destroy)
+        defaultButton = Button(createIntervalFrame, text="Default", command=lambda: self.callFunctionsForDefault(createIntervalFrame)) # lambda allows a callback function to take a parameter
         defaultButton.grid(row=1, column=0)
-        customButton = Button(createIntervalFrame, text="Custom", command=self.determineCustom and createIntervalFrame.destroy)
+        customButton = Button(createIntervalFrame, text="Custom", command=lambda: self.callFunctionsForCustom(createIntervalFrame))
         customButton.grid(row=1, column=1)
         createIntervalFrame.mainloop()
 
@@ -51,9 +51,23 @@ class WellRested:
 
         customFrame.mainloop()
 
+    def callFunctionsForDefault(self, frame): # This is a helper function called in createInterval to make sure that the beginDefault function gets called and then the window is destroyed
+        self.beginDefault()
+        frame.destroy()
+
+    def callFunctionsForCustom(self, frame): # This is a helper function called in createInterval to make sure that the determineCustom function gets called and then the window is destroyed
+        self.determineCustom()
+        frame.destroy()
+
     def beginDefault(self):
         initialTime = time.time()
         print(initialTime)
+
+    def pauseInterval(selfs):
+        print("Pause Interval")
+
+    def resetInterval(self):
+        print("Reset Interval")
 
 
 
